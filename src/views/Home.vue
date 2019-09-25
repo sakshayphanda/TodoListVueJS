115031d3cc00280b56a76bcd2d3e8b1b8d4694b0
sakshay phanda <sakshayphanda03@gmail.com>
2019-09-25 18:14:09 +0530

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld v-bind:msg="message"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'Hey this is hello to the world'
+    }
+  }
+}
+</script>
